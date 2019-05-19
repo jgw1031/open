d5372099b98e240d9271331c71dac1e8987c2af5
mandarin0567 <49848875+mandarin0567@users.noreply.github.com>
2019-05-19 23:39:48 +0900

    Add files via upload

---
+++ subactiviti.java
@@ -0,0 +1,20 @@
+package com.example.please;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*import android.view.View;
+import android.content.Intent;*/
+
+public class subactiviti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subactiviti);
+    }
+ /*   public void btn2(View v)
+    {
+        Intent intent2= new Intent(this,subactiviti.class);
+        startActivity(intent2);
+    }*/
+}
---
+++ therdlay.java
@@ -0,0 +1,13 @@
+package com.example.please;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class therdlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_therdlay);
+    }
+}
