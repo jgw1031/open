a17c321b927a44b9cc6f0ede10a494f543d197aa
jgw1031 <49265262+jgw1031@users.noreply.github.com>
2019-05-21 22:47:07 +0900

    Add files via upload

---
+++ 기본 데이터베이스 접속.php
@@ -0,0 +1,31 @@
+﻿<?php
+    $host = "localhost";      // ȣ��Ʈ �ּ�(localhost, 120.0.0.1)
+    $dbname = "dbroot";      // ����Ÿ ���̽�(DataBase) �̸�
+    $username = "root";          // DB ���̵�
+    $password = "1234";        // DB �н�����
+    $dbChar = "utf-8";            // ���� ���ڵ�
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SETNAMES utf-8');
+    try {
+        $con = new PDO("mysql:host={$host};dbname={$dbname};setnames=utf-8",$username, $password);
+    } catch(PDOException $e) {
+
+        die("Failed to connect to the database: " . $e->getMessage());
+    }
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+        function undo_magic_quotes_gpc(&$array) {
+            foreach($array as &$value) {
+                if(is_array($value)) {
+                    undo_magic_quotes_gpc($value);
+                }
+                else {
+                    $value = stripslashes($value);
+                }
+            }
+        }
+        undo_magic_quotes_gpc($_POST);
+        undo_magic_quotes_gpc($_GET);
+        undo_magic_quotes_gpc($_COOKIE);
+    }
+?>
---
+++ 찾기.php
@@ -0,0 +1,86 @@
+﻿<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+include('dbcon.php');
+
+
+
+//POST 값을 읽어온다.
+$ID=isset($_POST['country']) ? $_POST['country'] : '';
+$PASSWORD = isset($_POST['PASSWORD']) ? $_POST['PASSWORD'] : '';
+$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+
+
+if ($ID != "" ){
+
+    $sql="SELECT * FROM user where ID='$ID' OR PASSWORD ='$PASSWORD'";
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+
+    if ($stmt->rowCount() == 0){
+
+        echo "'";
+        echo $ID,", ",$PASSWORD;
+        echo "'은 찾을 수 없습니다.";
+    }
+	else{
+
+   		$data = array();
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+
+        	extract($row);
+
+            array_push($data,
+                array('ID'=>$row["ID"],
+                'NAME'=>$row["NAME"],
+                'PASSWORD'=>$row["PASSWORD"],
+	               'GENDER'=>$row["GENDER"],
+	                'PHONE'=>$row["PHONE"]
+            ));
+        }
+
+
+        if (!$android) {
+            echo "<pre>";
+            print_r($data);
+            echo '</pre>';
+        }else
+        {
+            $json = json_encode(array("webnautes"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+            echo $json;
+        }
+    }
+}
+else {
+    echo "검색할 나라를 입력하세요 ";
+}
+
+?>
+
+
+
+<?php
+
+$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+
+if (!$android){
+?>
+
+<html>
+   <body>
+
+      <form action="<?php $_PHP_SELF ?>" method="POST">
+         나라: <input type = "text" name = "country" />
+         이름: <input type = "text" name = "name" />
+         <input type = "submit" />
+      </form>
+
+   </body>
+</html>
+<?php
+}
+
+
+?>
